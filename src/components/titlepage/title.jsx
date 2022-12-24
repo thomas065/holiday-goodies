4ce07b9a003245442fb5a1e27efdc197f4d75258
Thomas Bell <thomasjbell065@gmail.com>
2022-12-23 22:59:58 -0500

    Login Button

--- src/components/titlepage/title.jsx
+++ src/components/titlepage/title.jsx
@@ -7,6 +7,9 @@ const Title = () => {
         <div className='holiday-font overlay'>
             <h1>Christmas Baking</h1>
             <h2>{bakingYear}</h2>
+            <button className='push-me button'>
+                <span className='front'>Login</span>
+            </button>
         </div>
     );
 };
