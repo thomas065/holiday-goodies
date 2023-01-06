438dd406bf98afb1b56728872f380d3363e3ec80
Thomas Bell <thomasjbell065@gmail.com>
2023-01-06 18:36:55 -0500

    major fixes to mobile views

---
+++ src/components/bakingInstructions/goodieCare.jsx
@@ -0,0 +1,12 @@
+import './goodieCare.scss';
+
+const GoodieCare = () => {
+  return (
+	<div>
+	  <h2>Happy Holidays!</h2>
+	  <p>Store your goodies in a cool, dry place.</p>
+	</div>
+  );
+}
+
+export default GoodieCare;
